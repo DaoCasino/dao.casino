14470bcd9b6f791555d98fa3e09be40387d1245e
Alexander Krupenkin <mail@akru.me>
2017-06-24 10:11:32 +0300

    Added softcap option by #11

--- contracts/DaoCasinoICO.sol
+++ contracts/DaoCasinoICO.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.9;
 
 import 'foundation/Crowdfunding.sol';
 
@@ -12,6 +12,7 @@ contract DaoCasinoICO is Crowdfunding {
      * @param _stopBlock Funding stop block nubmer
      * @param _minValue Minimal funded value in wei 
      * @param _maxValue Maximal funded value in wei
+     * @param _softcap  Softcap funded value in wei
      * @param _scale Bounty scaling factor by funded value
      * @param _startRatio Initial bounty ratio
      * @param _reductionStep Bounty reduction step in blocks 
@@ -26,11 +27,17 @@ contract DaoCasinoICO is Crowdfunding {
         uint256 _stopBlock,
         uint256 _minValue,
         uint256 _maxValue,
+        uint256 _softcap,
         uint256 _scale,
         uint256 _startRatio,
         uint256 _reductionStep,
         uint256 _reductionValue
-    ) Crowdfunding(_fund, _bounty, _reference, _startBlock, _stopBlock, _minValue, _maxValue, _scale, _startRatio, _reductionStep, _reductionValue) {}
+    ) Crowdfunding(_fund, _bounty, _reference, _startBlock, _stopBlock, _minValue, _maxValue, _scale, _startRatio, _reductionStep, _reductionValue) {
+        softcap = _softcap;
+    }
+
+    uint256 public softcap;
+    uint256 public softcapPassed = 0;
 
     // ONLY FOR 16.44s block time
     uint256 public constant BLOCKS_IN_DAY = 5256;
@@ -80,10 +87,34 @@ contract DaoCasinoICO is Crowdfunding {
      * @dev Crowdfunding success checks
      */
     modifier onlySuccess {
+                        // Normal termination
         bool isSuccess = totalFunded >= config.minValue
                       && block.number > config.stopBlock
-                      || totalFunded == config.maxValue;
+                        // Hardcap termination
+                      || totalFunded == config.maxValue
+                        // Softcap termination
+                      || softcapPassed > 0
+                      && block.number > (softcapPassed + BLOCKS_IN_DAY);
         if (!isSuccess) throw;
+        _;
+    }
+
+    /**
+     * @dev Crowdfunding running checks
+     */
+    modifier onlyRunning {
+        bool isRunning = totalFunded + msg.value <= config.maxValue
+                      && block.number >= config.startBlock
+                      && block.number <= config.stopBlock;
+        if (!isRunning) throw;
+
+        // Store softcap block 
+        if (totalFunded + msg.value > softcap && softcapPassed == 0)
+            softcapPassed = block.number;
+        // When softcap is activated - throw after one day
+        if (softcapPassed > 0 && block.number > (softcapPassed + BLOCKS_IN_DAY))
+            throw;
+
         _;
     }
 
@@ -100,7 +131,7 @@ contract DaoCasinoICO is Crowdfunding {
     function withdraw() onlySuccess {
         withdrawDone = true;
 
-        var bountyVal = bounty.totalSupply() / 70 * 30; 
+        var bountyVal = bounty.totalSupply() * 30 / 70; 
         bounty.emission(bountyVal);
         if (!bounty.transfer(fund, bountyVal)) throw;
     }
